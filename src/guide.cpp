0bb441632bf006bbffcdd6315462f8f36102a1f2
HaeRyong Jeong <jeong89@gooroom.kr>
2019-10-11 15:32:37 +0900

    Updating source regarding copyright
    Change-Id: I527c7433af26c184f052317e09d3dc2d94dd7b43

--- src/guide.cpp
+++ src/guide.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2015-2019 Gooroom <gooroom@gooroom.kr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 #include "guide-resources.h"
 
 #include <config.h>
@@ -354,6 +372,10 @@ main (int argc, char **argv)
                 return 0;
         }
     }
+
+    bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+    textdomain (GETTEXT_PACKAGE);
 
     GtkApplication *app;
 
