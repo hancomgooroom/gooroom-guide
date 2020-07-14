e71751ca93dafe730111ad38374133dbe330bd2a
Haeryong Jeong <jeong89@gooroom.kr>
2020-07-14 15:03:31 +0900

    Fixed window frame when 800x600 size
    Change-Id: Ib4d5b320d796d5025d52ad44d77ede6576257910

--- src/guide-window.c
+++ src/guide-window.c
@@ -37,12 +37,7 @@
 struct _GuideWindow
 {
   GtkApplicationWindow     parent;
-};
 
-typedef struct _GuideWindowPrivate GuideWindowPrivate;
-
-struct _GuideWindowPrivate
-{
   GtkWidget               *header_bar;
 
   GtkWidget               *bar_stack;
@@ -62,7 +57,7 @@ struct _GuideWindowPrivate
   bool                     is_begin;
 };
 
-G_DEFINE_TYPE_WITH_PRIVATE(GuideWindow, guide_window, GTK_TYPE_APPLICATION_WINDOW);
+G_DEFINE_TYPE(GuideWindow, guide_window, GTK_TYPE_APPLICATION_WINDOW);
 
 static void
 guide_window_check_show_at_begin (GtkButton* button, GuideWindow *window)
@@ -86,59 +81,55 @@ guide_window_check_show_at_begin (GtkButton* button, GuideWindow *window)
 }
 
 static void
-set_page_label (GuideWindowPrivate *priv)
+set_page_label (GuideWindow *self)
 {
     
-    gchar *page = g_strdup_printf ("%d / %d", priv->current, priv->total);
+    gchar *page = g_strdup_printf ("%d / %d", self->current, self->total);
 
-    gtk_label_set_label (GTK_LABEL (priv->page_label), page);
+    gtk_label_set_label (GTK_LABEL (self->page_label), page);
     g_free (page);
 }
 
 static void
-load_uri (int index, GuideWindowPrivate *priv)
+load_uri (int index, GuideWindow *self)
 {
     gchar *uri = NULL;
 
-    uri = g_array_index (priv->toc_array, gchar *, index);
-    priv->current = index;
+    uri = g_array_index (self->toc_array, gchar *, index);
+    self->current = index;
 
-    webkit_web_view_load_uri (priv->webview, uri);
+    webkit_web_view_load_uri (self->webview, uri);
 
-    set_page_label(priv);
+    set_page_label(self);
 }
 
 static void
 guide_window_clicked_prev (GtkButton* button, GuideWindow *self)
 {
-  GuideWindowPrivate *priv = guide_window_get_instance_private (self);
+  int index = self->current;
+  int total = self->total;
 
-  int index = priv->current;
-  int total = priv->total;
-
-  load_uri (--index, priv);
+  load_uri (--index, self);
 
   if (index == 0)
   {
-      gtk_stack_set_visible_child (GTK_STACK (priv->bar_stack), priv->start_bar);
+      gtk_stack_set_visible_child (GTK_STACK (self->bar_stack), self->start_bar);
   }
   else if (index == total)
-      gtk_stack_set_visible_child (GTK_STACK (priv->bar_stack), priv->content_bar);
+      gtk_stack_set_visible_child (GTK_STACK (self->bar_stack), self->content_bar);
 }
 
 static void
 guide_window_clicked_next (GtkButton* button, GuideWindow *self)
 {
-  GuideWindowPrivate *priv = guide_window_get_instance_private (self);
-
-  if (priv->current == 0)
-    gtk_stack_set_visible_child (GTK_STACK (priv->bar_stack), priv->content_bar);
+  if (self->current == 0)
+    gtk_stack_set_visible_child (GTK_STACK (self->bar_stack), self->content_bar);
 
   // If last page that disable next button
-  if (priv->current == priv->total)
-      gtk_stack_set_visible_child (GTK_STACK (priv->bar_stack), priv->end_bar);
+  if (self->current == self->total)
+      gtk_stack_set_visible_child (GTK_STACK (self->bar_stack), self->end_bar);
 
-  load_uri (++priv->current, priv);
+  load_uri (++self->current, self);
 }
 
 static gboolean
@@ -147,13 +138,19 @@ guide_window_webview_context_menu (WebKitWebView* web_view, WebKitContextMenu* c
     return true;
 }
 
+static gboolean
+guide_window_present (gpointer user_data)
+{
+  GuideWindow *window = GUIDE_WINDOW (user_data);
+  gtk_window_present (GTK_WINDOW (window));
+  return 0;
+}
+
 static void
 guide_window_webview_load_changed (WebKitWebView *web_view,
                        WebKitLoadEvent load_event,
                        GuideWindow *self)
 {
-  GuideWindowPrivate *priv = guide_window_get_instance_private (self);
-
   switch (load_event)
   {
     case WEBKIT_LOAD_COMMITTED:
@@ -166,7 +163,7 @@ guide_window_webview_load_changed (WebKitWebView *web_view,
       {
         // load end page
         webkit_web_view_stop_loading (web_view);
-        load_uri (priv->current, priv);
+        load_uri (self->current, self);
 
         // execute gooroom site
         g_spawn_command_line_async ("/usr/bin/gooroom-browser --new-window https://www.gooroom.kr", NULL);
@@ -194,11 +191,11 @@ guide_window_webview_load_changed (WebKitWebView *web_view,
     break;
     case WEBKIT_LOAD_FINISHED:
     {
-      if (priv->is_begin)
+      if (self->is_begin)
       {
-        priv->is_begin = FALSE;
+        self->is_begin = FALSE;
 
-        gtk_window_present (GTK_WINDOW (self));
+        g_timeout_add (100, guide_window_present, self);
       }
     }
     break;
@@ -210,7 +207,7 @@ guide_window_webview_load_changed (WebKitWebView *web_view,
 static void
 clear_tocs (gchar **item)
 {
-  g_free (item);
+  g_strfreev (item);
 }
 
 gchar*
@@ -218,7 +215,7 @@ get_language ()
 {
   gchar *locale = NULL;
   const gchar **split = NULL;
-  const gchar *result = NULL;
+  gchar *result = NULL;
   const g_autofree gchar *lang = NULL;
 
   locale = setlocale (LC_MESSAGES, NULL);
@@ -250,7 +247,7 @@ END:
 }
 
 static bool
-init_uri_list(GuideWindowPrivate *priv)
+init_uri_list(GuideWindow *self)
 {
   g_autofree gchar *toc_path = NULL;
   g_autofree gchar *toc = NULL;
@@ -259,7 +256,7 @@ init_uri_list(GuideWindowPrivate *priv)
   JsonObject *ro;
   JsonObjectIter iter;
   JsonArray *array;
-  g_autofree gchar *lang = get_language ();
+  gchar *lang = get_language ();
 
   int cnt = 0, length = 0;
 
@@ -280,17 +277,17 @@ init_uri_list(GuideWindowPrivate *priv)
   array = json_object_get_array_member (ro, "paths");
 
   length = json_array_get_length (array);
-  priv->total = length - 2;
+  self->total = length - 2;
 
-  priv->toc_array = g_array_new (TRUE, TRUE, sizeof (gchar *));
-  g_array_set_clear_func (priv->toc_array, (GDestroyNotify) clear_tocs);
+  self->toc_array = g_array_new (TRUE, TRUE, sizeof (gchar *));
+  g_array_set_clear_func (self->toc_array, (GDestroyNotify) clear_tocs);
 
   while (cnt < length)
   {
     const gchar *content = json_array_get_string_element (array, cnt);
     gchar *path = g_strdup_printf ("file://%s/%s/%s", toc_path, lang, content);
 
-    g_array_append_val (priv->toc_array, path);
+    g_array_append_val (self->toc_array, path);
 
     cnt++;
   }
@@ -301,23 +298,22 @@ init_uri_list(GuideWindowPrivate *priv)
 void
 guide_window_activate (GuideWindow *self)
 {
-  GuideWindowPrivate *priv = guide_window_get_instance_private (self);
   gboolean show_at_begin = FALSE;
   gchar *norun;
 
-  priv->is_begin = TRUE;
+  self->is_begin = TRUE;
 
-  gtk_header_bar_set_show_close_button (GTK_HEADER_BAR (priv->header_bar), TRUE);
+  gtk_header_bar_set_show_close_button (GTK_HEADER_BAR (self->header_bar), TRUE);
 
   // norun check
   norun = get_norun_file_path ();
   if (!g_file_test (norun, G_FILE_TEST_EXISTS))
       show_at_begin = TRUE;
 
-  init_uri_list(priv);
-  load_uri (0, priv);
+  init_uri_list(self);
+  load_uri (0, self);
 
-  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (priv->auto_start_check), show_at_begin);
+  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (self->auto_start_check), show_at_begin);
 }
 
 static void
@@ -354,7 +350,7 @@ guide_window_constructed (GObject *obj)
   gtk_widget_get_preferred_size (GTK_WIDGET (self), &req, NULL);
 
   if (geo.width <= req.width || geo.height <= req.height)
-    gtk_widget_set_size_request (GTK_WIDGET (self), MINIMUM_WIDTH, MINIMUM_HEIGHT);
+    gtk_widget_set_size_request (GTK_WIDGET (self->webview_frame), MINIMUM_WIDTH, MINIMUM_HEIGHT);
 }
 
 static void
@@ -373,15 +369,15 @@ guide_window_class_init (GuideWindowClass *class)
   gtk_widget_class_set_template_from_resource (widget_class, 
                                                "/org/gooroom/guide/guide.ui");
 
-  gtk_widget_class_bind_template_child_private (widget_class, GuideWindow, header_bar);
-  gtk_widget_class_bind_template_child_private (widget_class, GuideWindow, bar_stack);
-  gtk_widget_class_bind_template_child_private (widget_class, GuideWindow, start_bar);
-  gtk_widget_class_bind_template_child_private (widget_class, GuideWindow, content_bar);
-  gtk_widget_class_bind_template_child_private (widget_class, GuideWindow, end_bar);
-  gtk_widget_class_bind_template_child_private (widget_class, GuideWindow, auto_start_check);
-  gtk_widget_class_bind_template_child_private (widget_class, GuideWindow, page_label);
-  gtk_widget_class_bind_template_child_private (widget_class, GuideWindow, webview_frame);
-  gtk_widget_class_bind_template_child_private (widget_class, GuideWindow, webview);
+  gtk_widget_class_bind_template_child (widget_class, GuideWindow, header_bar);
+  gtk_widget_class_bind_template_child (widget_class, GuideWindow, bar_stack);
+  gtk_widget_class_bind_template_child (widget_class, GuideWindow, start_bar);
+  gtk_widget_class_bind_template_child (widget_class, GuideWindow, content_bar);
+  gtk_widget_class_bind_template_child (widget_class, GuideWindow, end_bar);
+  gtk_widget_class_bind_template_child (widget_class, GuideWindow, auto_start_check);
+  gtk_widget_class_bind_template_child (widget_class, GuideWindow, page_label);
+  gtk_widget_class_bind_template_child (widget_class, GuideWindow, webview_frame);
+  gtk_widget_class_bind_template_child (widget_class, GuideWindow, webview);
 
   gtk_widget_class_bind_template_callback (widget_class, guide_window_webview_context_menu);
   gtk_widget_class_bind_template_callback (widget_class, guide_window_webview_load_changed);
